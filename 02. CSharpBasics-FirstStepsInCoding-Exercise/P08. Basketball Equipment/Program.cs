768adcf2330feef88022ef2f88cd8ca14ca554e6
Petar Iliev <peshote@gmail.com>
2023-05-30 00:41:00 +0300

    Upload problem solved
    P08. Basketball Equipment

---
+++ 02. CSharpBasics-FirstStepsInCoding-Exercise/P08. Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int annualFee = int.Parse(Console.ReadLine());
+            double sneakersPrice = annualFee - (annualFee * 0.40);
+            double suitPrice = sneakersPrice - (sneakersPrice * 0.20);
+            double basketBallPrice = suitPrice / 4;
+            double accessoriesPrice = basketBallPrice / 5;
+            double expenses = annualFee + sneakersPrice + suitPrice + basketBallPrice + accessoriesPrice;
+            Console.WriteLine(expenses);
+        }
+    }
+}
