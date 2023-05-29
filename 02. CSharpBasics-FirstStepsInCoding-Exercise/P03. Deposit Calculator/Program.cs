3e50e6edfae1b760a0dd3db59b8aac4dadeff338
Petar Iliev <peshote@gmail.com>
2023-05-30 00:38:22 +0300

    Upload problem solved
    P03. Deposit Calculator

---
+++ 02. CSharpBasics-FirstStepsInCoding-Exercise/P03. Deposit Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double rate = double.Parse(Console.ReadLine());
+
+            double rates = rate/ 100;
+            double sum = deposit + months * ((deposit * rates)/12);
+            Console.WriteLine(sum);
+        }
+    }
+}
