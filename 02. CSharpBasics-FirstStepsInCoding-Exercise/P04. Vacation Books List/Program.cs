38a601cf94822e36e4e66f90129769ef3e5c1724
Petar Iliev <peshote@gmail.com>
2023-05-30 00:39:02 +0300

    Upload problem solved
    P04. Vacation Books List

---
+++ 02. CSharpBasics-FirstStepsInCoding-Exercise/P04. Vacation Books List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int allTime = allPages / pagesPerHour;
+            int pagesPerDay = allTime / days;
+            Console.WriteLine(pagesPerDay);
+        }
+    }
+}
