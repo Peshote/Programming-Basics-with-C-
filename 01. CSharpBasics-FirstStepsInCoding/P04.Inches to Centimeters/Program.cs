981c0a8015d623752409ef40a09d4182bc680dec
Petar Iliev <peshote@gmail.com>
2023-05-29 16:39:37 +0300

    Upload new problem solved
    P04.Inches to Centimeters

---
+++ 01. CSharpBasics-FirstStepsInCoding/P04.Inches to Centimeters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P04.Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = 2.54;
+
+            double result = a * b;
+            Console.WriteLine(result);
+        }
+    }
+}
