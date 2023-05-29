e3eee49b4653d1c7aec36a11e2c8a9e58e3eac6c
Petar Iliev <peshote@gmail.com>
2023-05-30 00:41:31 +0300

    Upload problem solved
    P09. Fish Tank

---
+++ 02. CSharpBasics-FirstStepsInCoding-Exercise/P09. Fish Tank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P09._Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aquariumLength = int.Parse(Console.ReadLine());
+            int aquariumWidth = int.Parse(Console.ReadLine());
+            int aquariumHeight = int.Parse(Console.ReadLine());
+            double occupiedSpace = double.Parse(Console.ReadLine());
+            double occupiedSpacePercent = occupiedSpace / 100;
+            int aquariumVolume = aquariumLength * aquariumWidth * aquariumHeight;
+            double aquaruiumVolumeLiters = aquariumVolume * 0.001;
+            double reqWaterAmount = aquaruiumVolumeLiters * (1 - occupiedSpacePercent);
+            Console.WriteLine(reqWaterAmount);
+        }
+    }
+}
