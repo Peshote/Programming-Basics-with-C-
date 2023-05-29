48c65fd6b7c2f9a9f4b8581a85bd96621a3a5afc
Petar Iliev <peshote@gmail.com>
2023-05-29 16:44:04 +0300

    Upload new problem solved
    P08. Pet Shop

---
+++ 01. CSharpBasics-FirstStepsInCoding/P08. Pet Shop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dog = int.Parse(Console.ReadLine());
+            int cat = int.Parse(Console.ReadLine());
+            double dogFood = 2.50;
+            int catFood = 4;
+            double sum = (dog * dogFood + cat * catFood);
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
