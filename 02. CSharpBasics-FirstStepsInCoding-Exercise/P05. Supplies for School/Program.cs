c243c0caf6883a4f07ab7d3af4f2a969cf7900e6
Petar Iliev <peshote@gmail.com>
2023-05-30 00:39:33 +0300

    Upload problem solved
    P05. Supplies for School

---
+++ 02. CSharpBasics-FirstStepsInCoding-Exercise/P05. Supplies for School/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P05._Supplies_for_School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pens = 5.80;
+            double markers = 7.20;
+            double detergent = 1.20;
+            int countPens = int.Parse(Console.ReadLine());
+            int countMarkers = int.Parse(Console.ReadLine());
+            int countDetergent = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+            double pricePens = pens * countPens;
+            double priceMarkers = markers * countMarkers;
+            double priceDetergent = detergent * countDetergent;
+            double allDiscount = discount / 100;
+            double allPrice = pricePens + priceMarkers + priceDetergent;
+            double finalPrice = allPrice - (allPrice * allDiscount);
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
