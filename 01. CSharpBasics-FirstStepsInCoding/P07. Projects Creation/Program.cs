1d3165bb794b8397730fb5c021afae619daf58ce
Petar Iliev <peshote@gmail.com>
2023-05-29 16:43:30 +0300

    Upload new problem solved
    P07. Projects Creation

---
+++ 01. CSharpBasics-FirstStepsInCoding/P07. Projects Creation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P07._Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int project = int.Parse(Console.ReadLine());
+            int hour = 3;
+            int projects = project * hour;
+
+            Console.WriteLine($"The architect {name} will need {projects} hours to complete {project} project/s.");
+        }
+    }
+}
