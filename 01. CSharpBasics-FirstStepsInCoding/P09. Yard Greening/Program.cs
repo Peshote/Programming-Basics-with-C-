663cb150ebdc37920d6774903c27dddabca560ec
Petar Iliev <peshote@gmail.com>
2023-05-29 16:44:40 +0300

    Upload new problem solved
    P09. Yard Greening

---
+++ 01. CSharpBasics-FirstStepsInCoding/P09. Yard Greening/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P09._Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumM2 = double.Parse(Console.ReadLine());
+            double M2 = 7.61;
+            double discount = 0.18;
+            double sum1 = sumM2 * M2;
+            double sum2 = sum1 * discount;
+            double endSum = sum1 - sum2;
+            Console.WriteLine($"The final price is: {endSum} lv.");
+            Console.WriteLine($"The discount is: {sum2} lv.");
+        }
+    }
+}
