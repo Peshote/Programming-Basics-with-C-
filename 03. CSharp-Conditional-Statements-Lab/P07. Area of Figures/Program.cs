58b54bb46f0a1f811ae3ccf0fe1377cb62f61493
Petar Iliev <peshote@gmail.com>
2023-05-31 02:31:57 +0300

    Upload new problem solved
    P07. Area of Figures

---
+++ 03. CSharp-Conditional-Statements-Lab/P07. Area of Figures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P07._Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string shapeType = Console.ReadLine();
+
+            double area = 0;
+
+            if (shapeType == "square") 
+            {
+                double sideLength = double.Parse(Console.ReadLine());
+                area = sideLength * sideLength;                
+            }
+            else if (shapeType == "rectangle")
+            {
+                double sideLengthA = double.Parse(Console.ReadLine());
+                double sideLengthB = double.Parse(Console.ReadLine());
+                area = sideLengthA * sideLengthB;
+            }
+            else if (shapeType == "circle") 
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * radius * radius;
+            }
+            else if (shapeType == "triangle") 
+            {
+                double sideLength = double.Parse(Console.ReadLine());
+                double heightLength = double.Parse(Console.ReadLine());
+                area = sideLength * heightLength / 2;
+            }
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}
