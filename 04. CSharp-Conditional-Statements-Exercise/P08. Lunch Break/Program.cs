d43a83110aac5a5f22711a7287cf2019a9df4c8f
Petar Iliev <peshote@gmail.com>
2023-06-01 19:51:10 +0300

    Upload new problem solved
    P08. Lunch Break

---
+++ 04. CSharp-Conditional-Statements-Exercise/P08. Lunch Break/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serialName = Console.ReadLine();
+            int epTime = int.Parse(Console.ReadLine());
+            int breakTime = int.Parse(Console.ReadLine());
+            double lunchTime = (double) breakTime * 1 / 8;
+            double relaxTime = (double) breakTime * 1 / 4;
+            double leftTime = breakTime - lunchTime - relaxTime;
+            if (leftTime >= epTime) 
+            {
+                Console.WriteLine($"You have enough time to watch {serialName} and left with {Math.Ceiling(leftTime - epTime)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {serialName}, you need {Math.Ceiling(epTime - leftTime)} more minutes.");
+            }
+        }
+    }
+}
