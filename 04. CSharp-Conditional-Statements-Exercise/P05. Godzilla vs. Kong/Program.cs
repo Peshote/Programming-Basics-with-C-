b8b7f6b32c2190b0f11a9ae7f6623e7815789f5b
Petar Iliev <peshote@gmail.com>
2023-06-01 19:49:59 +0300

    Upload new problem solved
    P05. Godzilla vs. Kong

---
+++ 04. CSharp-Conditional-Statements-Exercise/P05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P05._Godzilla_vs._Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budged = double.Parse(Console.ReadLine());
+            int extrasCount = int.Parse(Console.ReadLine());
+            double clothingPrice = double.Parse(Console.ReadLine());
+            double decor = budged * 0.10;
+            
+            if (extrasCount > 150)
+            {
+                clothingPrice -= clothingPrice * 0.10;
+            }
+            double expenses = extrasCount * clothingPrice + decor;
+            if (expenses > budged)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {expenses - budged:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budged - expenses:f2} leva left.");
+            }
+        }
+    }
+}
