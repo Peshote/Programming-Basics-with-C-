6d9862600088287253308f0100461d86ef588560
Petar Iliev <peshote@gmail.com>
2023-06-02 22:56:25 +0300

    Upload new problem solved
    P07.Working Hours

---
+++ 05. CSharp-Nested-Conditional-Statements-Lab/P07.Working Hours/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P07.Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourTime = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (hourTime >= 10 && hourTime <= 18)
+            {
+                if (day != "Sunday")
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
