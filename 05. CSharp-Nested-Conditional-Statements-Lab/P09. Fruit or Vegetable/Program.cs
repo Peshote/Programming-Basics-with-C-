4adb6af799f45eb709167824a86a18f18e287aff
Petar Iliev <peshote@gmail.com>
2023-06-02 22:57:17 +0300

    Upload new problem solved
    P09. Fruit or Vegetable

---
+++ 05. CSharp-Nested-Conditional-Statements-Lab/P09. Fruit or Vegetable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P09._Fruit_or_Vegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName = Console.ReadLine();
+
+            if (productName == "banana" || productName == "apple" || productName == "kiwi" || productName == "cherry" || productName == "lemon" || productName == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (productName == "tomato" || productName == "cucumber" || productName == "pepper" || productName == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");            
+            }
+        }
+    }
+}
