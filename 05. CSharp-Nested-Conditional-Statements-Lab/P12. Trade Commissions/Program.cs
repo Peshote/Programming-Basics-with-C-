ecac90a1e078fcf6eeb5e67c2f81ae11229e3a54
Petar Iliev <peshote@gmail.com>
2023-06-02 22:58:31 +0300

    Upload new problem solved
    P12. Trade Commissions

---
+++ 05. CSharp-Nested-Conditional-Statements-Lab/P12. Trade Commissions/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace P12._Trade_Commissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double salesVol = double.Parse(Console.ReadLine());
+            double commission = 0.00;
+            if (town == "Sofia")
+            {
+                if (salesVol >= 0 && salesVol <= 500)
+                {
+                    commission = 0.05;
+                }
+                else if (salesVol > 500 && salesVol <= 1000)
+                {
+                    commission = 0.07;
+                }
+                else if (salesVol > 1000 && salesVol <= 10000)
+                {
+                    commission = 0.08;
+                }
+                else if (salesVol > 10000)
+                {
+                    commission = 0.12;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (town == "Varna")
+            {
+                if (salesVol >= 0 && salesVol <= 500)
+                {
+                    commission = 0.045;
+                }
+                else if (salesVol > 500 && salesVol <= 1000)
+                {
+                    commission = 0.075;
+                }
+                else if (salesVol > 1000 && salesVol <= 10000)
+                {
+                    commission = 0.10;
+                }
+                else if (salesVol > 10000)
+                {
+                    commission = 0.13;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (town == "Plovdiv")
+            {
+                if (salesVol >= 0 && salesVol <= 500)
+                {
+                    commission = 0.055;
+                }
+                else if (salesVol > 500 && salesVol <= 1000)
+                {
+                    commission = 0.08;
+                }
+                else if (salesVol > 1000 && salesVol <= 10000)
+                {
+                    commission = 0.12;
+                }
+                else if (salesVol > 10000)
+                {
+                    commission = 0.145;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            if (commission != 0)
+            {
+                Console.WriteLine($"{salesVol * commission:f2}");
+            }
+        }
+    }
+}
