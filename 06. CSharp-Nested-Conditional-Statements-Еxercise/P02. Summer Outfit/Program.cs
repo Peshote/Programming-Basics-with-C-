24f8e754988f3773c5bac329d341ec6414deda23
Petar Iliev <peshote@gmail.com>
2023-06-03 01:32:09 +0300

    Upload new problem solved
    P02. Summer Outfit

---
+++ 06. CSharp-Nested-Conditional-Statements-Еxercise/P02. Summer Outfit/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text.Unicode;
+
+namespace P02._Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+
+            if (temp >= 10 && temp <= 18)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (timeOfDay == "Afternoon" || timeOfDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else if (temp > 18 && temp <= 24)
+            {
+                if (timeOfDay == "Afternoon")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfDay == "Morning" || timeOfDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else if (temp >= 25)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfDay == "Afternoon")
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                else if (timeOfDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            Console.WriteLine($"It's {temp} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
