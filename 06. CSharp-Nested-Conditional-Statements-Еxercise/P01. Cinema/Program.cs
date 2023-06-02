fae956b93d7720eb1882a5e082375b81904fed24
Petar Iliev <peshote@gmail.com>
2023-06-03 01:31:42 +0300

    Upload new problem solved
    P01. Cinema

---
+++ 06. CSharp-Nested-Conditional-Statements-Еxercise/P01. Cinema/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P01._Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectionType = Console.ReadLine();
+            int rowCount = int.Parse(Console.ReadLine());
+            int columnCount = int.Parse(Console.ReadLine());
+            double income = 0.0;
+            if (projectionType == "Premiere")
+            {
+                income = rowCount * columnCount * 12.00;
+            }
+            else if (projectionType == "Normal")
+            {
+                income = rowCount * columnCount * 7.50;
+            }
+            else if (projectionType == "Discount")
+            {
+                income = rowCount * columnCount * 5.00;
+            }
+            Console.WriteLine("{0:f2} leva", income);
+        }
+    }
+}
