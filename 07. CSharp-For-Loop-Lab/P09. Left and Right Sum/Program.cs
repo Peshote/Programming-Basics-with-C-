77459d6e636eecc3b1b05b1ec2d9d29a2dcf7ece
Petar Iliev <peshote@gmail.com>
2023-06-04 22:11:20 +0300

    Upload new problem solved
    P09. Left and Right Sum

---
+++ 07. CSharp-For-Loop-Lab/P09. Left and Right Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P09._Left_and_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());            
+            int sum1 = 0;
+            int sum2 = 0;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                sum1 += num1;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                int num2 = int.Parse(Console.ReadLine());
+                sum2 += num2;
+            }
+            if(sum1 == sum2)
+            {
+                Console.WriteLine($"Yes, sum = {sum1}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(sum1 - sum2)}");
+            }
+        }
+    }
+}
