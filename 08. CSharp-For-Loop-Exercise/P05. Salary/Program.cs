d94d7cd0351a34863e0ba8de1457a0c4f4c333f9
Petar Iliev <peshote@gmail.com>
2023-06-05 22:58:48 +0300

    Upload new problem solved
    P05. Salary

---
+++ 08. CSharp-For-Loop-Exercise/P05. Salary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P05._Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int openTabsCount = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            string siteName = "";            
+            for (int i = 0; i <= openTabsCount && salary > 0; i++)
+            {
+                siteName = Console.ReadLine();
+                if (siteName == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (siteName == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (siteName == "Reddit")
+                {
+                    salary -= 50;
+                }
+                else
+                {
+                    salary -= 0;
+                }
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine($"{salary}");
+            }
+        }
+    }
+}
