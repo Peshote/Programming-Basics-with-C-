ad8cf232f949f8e1a70efcdb7092081e1282099d
Petar Iliev <peshote@gmail.com>
2023-06-05 22:59:10 +0300

    Upload new problem solved
    P06. Oscars

---
+++ 08. CSharp-For-Loop-Exercise/P06. Oscars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P06._Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double academyPoints = double.Parse(Console.ReadLine());
+            int juryCount = int.Parse(Console.ReadLine());
+            double actorPoints = academyPoints;
+            
+            for (int i = 1; i <= juryCount && actorPoints <= 1250.5; i++)
+            {
+                string juryName = Console.ReadLine();
+                double juryPoints = double.Parse(Console.ReadLine());
+                int lenght = juryName.Length;
+                double finalPoints = ((lenght * juryPoints) / 2);
+                actorPoints += finalPoints;
+            }
+            if (actorPoints > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {actorPoints:f1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {1250.5 - actorPoints:f1} more!");
+            }
+        }
+    }
+}
