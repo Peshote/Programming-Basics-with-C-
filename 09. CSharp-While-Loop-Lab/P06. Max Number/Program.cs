7634794b095c39f8b251148c60eb4e734e1314aa
Petar Iliev <peshote@gmail.com>
2023-06-06 22:29:47 +0300

    Upload new problems solved
    P06. Max Number

---
+++ 09. CSharp-While-Loop-Lab/P06. Max Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNum = int.MinValue;
+            string input;
+
+            while ((input = Console.ReadLine()) != "Stop")
+            {
+                int number = int.Parse(input);
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+            }
+            Console.WriteLine(maxNum.ToString());
+        }
+    }
+}
