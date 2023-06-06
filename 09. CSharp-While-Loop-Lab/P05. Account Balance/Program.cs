50a3eeb5a52adbb26e260e18ee94a81d355b9c98
Petar Iliev <peshote@gmail.com>
2023-06-06 22:29:27 +0300

    Upload new problem solved
    P05. Account Balance

---
+++ 09. CSharp-While-Loop-Lab/P05. Account Balance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyAccount = 0;
+            string deposit;
+
+            while ((deposit = Console.ReadLine()) != "NoMoreMoney")
+            {
+                double depositAmount = double.Parse(deposit);
+                if (depositAmount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                moneyAccount += depositAmount;
+                Console.WriteLine($"Increase: {depositAmount:f2}");
+            }
+            Console.WriteLine($"Total: {moneyAccount:f2}");
+        }
+    }
+}
