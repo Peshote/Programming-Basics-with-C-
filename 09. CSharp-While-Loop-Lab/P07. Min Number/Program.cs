e4e3d76656e40a792fbe565c82c7262ab2e0f7dd
Petar Iliev <peshote@gmail.com>
2023-06-06 22:30:10 +0300

    Upload new problem solved
    P07. Min Number

---
+++ 09. CSharp-While-Loop-Lab/P07. Min Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNum = int.MaxValue;
+            string input;
+
+            while ((input = Console.ReadLine()) != "Stop")
+            {
+                int number = int.Parse(input);
+                if (number < minNum)
+                {
+                    minNum = number;
+                }
+            }
+            Console.WriteLine(minNum.ToString());
+        }
+    }
+}
