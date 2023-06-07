ce6b5b6b34aed9bb2d479b75333d0ae2259fe42b
Petar Iliev <peshote@gmail.com>
2023-06-07 21:14:29 +0300

    Upload new problem solved
    P06. Cake

---
+++ 10 .CSharp-While-Loop-Exercise/P06. Cake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeLength = int.Parse(Console.ReadLine());
+            int cakePieces = cakeWidth * cakeLength;
+            string input;
+
+            while (cakePieces > 0)
+            {
+                input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    break;
+                }
+                else
+                {
+                    cakePieces -= int.Parse(input);
+                }
+            }
+            if (cakePieces > 0)
+            {
+                Console.WriteLine($"{cakePieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(cakePieces)} pieces more.");
+            }
+        }
+    }
+}
