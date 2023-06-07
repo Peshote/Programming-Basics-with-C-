90b7d72f8cc494ac345c61c44faf30c566f95907
Petar Iliev <peshote@gmail.com>
2023-06-07 21:12:56 +0300

    Upload new problem solved
    P02. Exam Preparation

---
+++ 10 .CSharp-While-Loop-Exercise/P02. Exam Preparation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P02._Exam_Preparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowScores = int.Parse(Console.ReadLine());
+            int lowScoreTimes = 0;
+            int finishedTaskCount = 0;
+            double scoresSum = 0;
+            string lastTask = "";
+            bool isFailed = true;
+            while (lowScoreTimes < lowScores)
+            {
+                string taskName = Console.ReadLine();
+                if (taskName == "Enough")
+                {
+                    isFailed = false;
+                    break;
+                }
+                int score = int.Parse(Console.ReadLine());
+                if (score <= 4)
+                {
+                    lowScoreTimes++;
+                }
+                scoresSum += score;
+                finishedTaskCount++;
+                lastTask = taskName;
+            }
+            if (isFailed)
+            {
+                Console.WriteLine($"You need a break, {lowScores} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {scoresSum / finishedTaskCount:f2}");
+                Console.WriteLine($"Number of problems: {finishedTaskCount}");
+                Console.WriteLine($"Last problem: {lastTask}");
+            }
+        }
+    }
+}
