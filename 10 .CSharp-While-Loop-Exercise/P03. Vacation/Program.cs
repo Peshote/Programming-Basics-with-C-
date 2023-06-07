719d8fc58a6b755f22fc0a788ab1e4c45622053b
Petar Iliev <peshote@gmail.com>
2023-06-07 21:13:20 +0300

    Upload new problem solved
    P03. Vacation

---
+++ 10 .CSharp-While-Loop-Exercise/P03. Vacation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace P03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vacationCost = double.Parse(Console.ReadLine());
+            double availableMoney = double.Parse(Console.ReadLine());
+
+            int daysCounter = 0;
+            int spendingCounter = 0;
+
+            while (availableMoney < vacationCost && spendingCounter < 5)
+            {
+                string action = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+                daysCounter++;
+                if (action == "save")
+                {
+                    availableMoney += money;
+                    spendingCounter = 0;
+                }
+                else
+                {
+                    availableMoney -= money;                    
+                    spendingCounter++;                    
+                }
+                if (availableMoney < 0)
+                {
+                    availableMoney = 0;
+                }                    
+            }
+            if (spendingCounter == 5)
+            {
+                Console.WriteLine($"You can't save the money.");
+                Console.WriteLine(daysCounter);
+            }
+            if (availableMoney >= vacationCost)
+            {
+                Console.WriteLine($"You saved the money for {daysCounter} days.");
+            }
+        }
+    }
+}
