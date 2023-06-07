e367681dfb10f7834320e7b264a0be31693126cd
Petar Iliev <peshote@gmail.com>
2023-06-07 21:14:52 +0300

    Upload new problem solved
    P07. Moving

---
+++ 10 .CSharp-While-Loop-Exercise/P07. Moving/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int spaceWidth = int.Parse(Console.ReadLine());
+            int spaceLength = int.Parse(Console.ReadLine());
+            int spaceHeight = int.Parse(Console.ReadLine());
+            int spaceVolume = spaceWidth * spaceLength * spaceHeight;
+            string input;
+
+            while (spaceVolume > 0)
+            {
+                input = Console.ReadLine();
+                if (input == "Done")
+                {
+                    break;
+                }
+                else
+                {
+                    spaceVolume -= int.Parse(input);
+                }
+            }
+            if (spaceVolume > 0)
+            {
+                Console.WriteLine($"{spaceVolume} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(spaceVolume)} Cubic meters more.");
+            }
+        }
+    }
+}
