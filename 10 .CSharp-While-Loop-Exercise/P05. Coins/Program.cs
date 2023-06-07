d1421c9096e58a9f47348be9d8d5d4c843313b2d
Petar Iliev <peshote@gmail.com>
2023-06-07 21:14:08 +0300

    Upload new problem solved
    P05. Coins

---
+++ 10 .CSharp-While-Loop-Exercise/P05. Coins/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace P05._Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double changeSum = double.Parse(Console.ReadLine())*100;
+            int coinsCounter = 0;
+            while (changeSum > 0)
+            {
+                if (changeSum >= 200)
+                {
+                    changeSum -= 200;
+                }
+                else if (changeSum >= 100)
+                {
+                    changeSum -= 100;
+                }
+                else if (changeSum >= 50)
+                {
+                    changeSum -= 50;
+                }
+                else if (changeSum >= 20)
+                {
+                    changeSum -= 20;
+                }
+                else if (changeSum >= 10)
+                {
+                    changeSum -= 10;
+                }
+                else if (changeSum >= 5)
+                {
+                    changeSum -= 5;
+                }
+                else if (changeSum >= 2)
+                {
+                    changeSum -= 2;
+                }
+                else if (changeSum >= 1)
+                {
+                    changeSum -= 1;
+                }
+                else
+                {
+                    changeSum -= 0;
+                    break;
+                }
+                coinsCounter++;
+            }
+            Console.WriteLine(coinsCounter);
+        }
+    }
+}
