91c4c2f50919a3e5c3f2a9a8b6edff123368da96
Petar Iliev <peshote@gmail.com>
2023-06-07 21:13:46 +0300

    Upload new problem solved
    P04. Walking

---
+++ 10 .CSharp-While-Loop-Exercise/P04. Walking/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P04._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stepsSum = 0;            
+            string input;
+            while (stepsSum < 10000)
+            {
+                input = Console.ReadLine();
+                if (input == "Going home")
+                {
+                    stepsSum += int.Parse(Console.ReadLine());
+                    break;
+                }
+                stepsSum += int.Parse(input);                
+            }
+            if (stepsSum >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{stepsSum - 10000} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - stepsSum} more steps to reach goal.");
+            }
+        }
+    }
+}
