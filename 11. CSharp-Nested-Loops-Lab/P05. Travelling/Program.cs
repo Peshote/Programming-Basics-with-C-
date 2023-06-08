75a7605537400c5011714ee1de9e762d810b0505
Petar Iliev <peshote@gmail.com>
2023-06-08 23:51:56 +0300

    Upload new problem solved
    P05. Travelling

---
+++ 11. CSharp-Nested-Loops-Lab/P05. Travelling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string destination;
+            double minimumBudged;                     
+
+            input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                destination = input;
+                minimumBudged = double.Parse(Console.ReadLine());
+
+                while (minimumBudged > 0)
+                {                    
+                    minimumBudged -= double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Going to {destination}!");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
