2c1d1ef6333fac4ce5302f05458d7f94bb363585
Petar Iliev <peshote@gmail.com>
2023-06-09 00:10:04 +0300

    Upload new problem solved
    P05. Special Numbers

---
+++ 12. CSharp-Nested-Loops-Exercise/P05. Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            
+            for (int l = 1; l <= 9; l++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int i = 1; i <= 9; i++)
+                        {
+                            if (num % l == 0 && num % j == 0 && num % k == 0 && num % i == 0)
+                            {
+                                Console.Write($"{l}{j}{k}{i} ");
+                            }                            
+                        }
+                    }            
+                }
+            }
+        }
+    }
+}
