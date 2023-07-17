7b40d171a0a2d26d60a039e5bd80de3de3ea917a
Petar Iliev <peshote@gmail.com>
2023-07-17 22:00:53 +0300

    Upload new problem solved
    P04. Grandpa Stavri

---
+++ 13. Programming Basics Online Pre - Exam - 18 and 19 February 2023/P04. Grandpa Stavri/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P04._Grandpa_Stavri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bakingDays = int.Parse(Console.ReadLine());
+            double rakiaTotalQuan = 0;
+            double rakiaTotalDegrees = 0;
+            double averageRakiaDegrees = 0;
+            for (int i = 0; i < bakingDays; i++)
+            {
+                double rakiaQuanPerDay = double.Parse(Console.ReadLine());
+                double rakiaDegreesPerDay = double.Parse(Console.ReadLine());
+                rakiaTotalQuan += rakiaQuanPerDay;
+                rakiaTotalDegrees += rakiaDegreesPerDay * rakiaQuanPerDay;
+            }
+            averageRakiaDegrees = rakiaTotalDegrees / rakiaTotalQuan;
+
+            Console.WriteLine($"Liter: {rakiaTotalQuan:f2}");
+            Console.WriteLine($"Degrees: {averageRakiaDegrees:f2}");
+            if (averageRakiaDegrees < 38)
+            {
+                Console.WriteLine($"Not good, you should baking!");
+            }
+            else if (averageRakiaDegrees >= 38 && averageRakiaDegrees <= 42)
+            {
+                Console.WriteLine($"Super!");
+            }
+            else
+            {
+                Console.WriteLine($"Dilution with distilled water!");
+            }
+        }
+    }
+}
