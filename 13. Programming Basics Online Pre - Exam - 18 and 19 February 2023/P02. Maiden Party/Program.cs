07b6eb08c608980b52ae6172d6e0d5d7b27c1a0d
Petar Iliev <peshote@gmail.com>
2023-07-17 22:00:11 +0300

    Upload new problem solved
    P02. Maiden Party

---
+++ 13. Programming Basics Online Pre - Exam - 18 and 19 February 2023/P02. Maiden Party/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P02._Maiden_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double loveMessagePrice = 0.60;
+            double waxRosePrice = 7.20;
+            double keyChainPrice = 3.60;
+            double caricaturePrice = 18.20;
+            double luckySurprisePrice = 22;
+
+            double maidenPartyPrice = double.Parse(Console.ReadLine());
+            int loveMessagesQuan = int.Parse(Console.ReadLine());
+            int waxRosesQuan = int.Parse(Console.ReadLine());
+            int keyChainsQuan = int.Parse(Console.ReadLine());
+            int caricaturesQuan = int.Parse(Console.ReadLine());
+            int luckySurprisesQuan = int.Parse(Console.ReadLine());
+
+            double orderSum = loveMessagesQuan * loveMessagePrice + waxRosesQuan * waxRosePrice + keyChainsQuan * keyChainPrice + caricaturesQuan * caricaturePrice + luckySurprisesQuan * luckySurprisePrice;
+            int orderItemsQuan = loveMessagesQuan + waxRosesQuan + keyChainsQuan + caricaturesQuan + luckySurprisesQuan;
+
+            if (orderItemsQuan >= 25)
+            {
+                orderSum -= orderSum * 0.35;
+            }
+            double finalIncome = orderSum -= orderSum * 0.10;
+            if (finalIncome >= maidenPartyPrice)
+            {
+                Console.WriteLine($"Yes! {finalIncome - maidenPartyPrice:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {Math.Abs(finalIncome - maidenPartyPrice):f2} lv needed.");
+            }
+        }   
+    }
+}
