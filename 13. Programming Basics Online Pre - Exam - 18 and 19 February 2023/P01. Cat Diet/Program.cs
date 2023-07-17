34c6dc7991928d9ff7995042c4017f26989cc8a5
Petar Iliev <peshote@gmail.com>
2023-07-17 21:59:32 +0300

    Upload new problem solved
    P01. Cat Diet

---
+++ 13. Programming Basics Online Pre - Exam - 18 and 19 February 2023/P01. Cat Diet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01._Cat_Diet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double caloriesFats = 9;
+            double caloriesProteins = 4;
+            double caloriesCarbohydrates = 4;
+
+            double fatsPercent = double.Parse(Console.ReadLine());
+            double proteinsPercent = double.Parse(Console.ReadLine());
+            double carbohydratesPercent = double.Parse(Console.ReadLine());
+            double totalCalories = double.Parse(Console.ReadLine());
+            double waterPercent = double.Parse(Console.ReadLine());
+
+            double totalFatsWeight = ((fatsPercent / 100) * totalCalories) / caloriesFats;
+            double totalProteinsWeight = ((proteinsPercent / 100) * totalCalories) / caloriesProteins;
+            double totalCarbohydratesWeight = ((carbohydratesPercent / 100) * totalCalories) / caloriesCarbohydrates;
+            double totalFoodWeight = totalFatsWeight + totalProteinsWeight+ totalCarbohydratesWeight;
+            double caloreisPerFoodGram = totalCalories / totalFoodWeight;
+            double caloriesPercentWithoutWater = (waterPercent / 100) * caloreisPerFoodGram;
+            double finalCalories = caloreisPerFoodGram - caloriesPercentWithoutWater;
+            Console.WriteLine($"{finalCalories:f4}");
+        }
+    }
+}
